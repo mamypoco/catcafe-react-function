bfcb31a87f38ccac534b015ffe033e37041baf2f
Mami Nishiwaki <mamypoco@gmail.com>
2022-10-12 15:41:25 -0700

    removed / home router

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -12,7 +12,7 @@ const Main = () => {
       <div>
          <Header />
          <Routes>
-            <Route path="/" element={<Home />} />
+            {/* <Route path="/" element={<Home />} /> */}
             <Route path="/home" element={<Home />} />
             <Route path="/aboutus" element={<About />} />
             <Route path="/cafemenu" element={<Cafemenu />} />
