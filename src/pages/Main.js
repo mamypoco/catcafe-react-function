98462d66652baf1b978e999539fb91f71e45a69a
Mami Nishiwaki <mamypoco@gmail.com>
2022-10-13 12:54:01 -0700

    update

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,7 +2,7 @@ import Home from "./Home";
 import Header from "./Header";
 import Footer from "./Footer";
 import Contact from "./Contact";
-import Cafemenu from "./Cafemenu";
+import Cafemenu from "../pages/Cafemenu";
 import Adoptcat from "./Adoptcat";
 import About from "./About";
 import { Routes, Route } from "react-router-dom";
